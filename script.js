3f8d2e8274802f458ae4dd20ee2dd288273067bd
Harrison Armitage <38173749+HarryHighPants@users.noreply.github.com>
2024-03-18 23:54:30 +1000

    poem spiral

--- script.js
+++ script.js
@@ -7,11 +7,36 @@
 // Spirals with eyes so full
 // Pharyngeal minds observe
 // Our machines awake
-
-const stardiscrete=(n,e,i=.2,a=10)=>{let $=Math.
-sin(e*i),r=Math.abs(Math.round($*a+10)),
-s=$<Math.sin((e+1)*i),o=Math.abs($)>.85?"*":s?"-":"@";
-return n.substring(0,r)+o+n.substring(r+1)};
-for(let frame=0;;frame++){let n=Array(25).join(" ");
-n=stardiscrete(n,frame+15,.2,10),n=stardiscrete(n,frame,.21,10),
-console.log(n),await new Promise(n=>setTimeout(n,60))}
+let                     spiral
+=(e,                     a)=>
+ {let                     i=
+  Math                  .sin
+   (a),                 n=
+      Math          .round
+        (10         *i+
+          10)    ,r=
+             Math
+          .abs   (i)
+        >.85        ?"*"
+     :Math           .cos
+    (a)                 >0?
+   "-"                   :"@"
+ return                    e.
+ slice                    (0,n
+)+r                         +e
+.slice                    (n+1)
+ };for                    (let
+  frame                  =0;;
+   frame++              ){
+     await            new
+       Promise     ((e)=>
+           setTimeout
+             (e,60))
+           let     e=
+        spiral       (" ".
+     repeat            (25),
+   (frame                +15)
+  *.2);                  console
+.log(                      spiral
+(e,                          .21*                  
+frame                         ))}
--- spiral.ts
+++ spiral.ts
@@ -1,18 +1,12 @@
-let addSinToLine = (
-  line: string,
-  frame: number,
-  speed: number,
-  intensity: number
-) => {
-  let sin = Math.sin(frame * speed)
-  let x = Math.round(sin * intensity + 10)
-  let asc = sin < Math.sin((frame + 1) * speed)
-  let char = sin > 0.85 ? "*" : asc ? "-" : "@"
+let addSinToLine = (line: string, frame: number) => {
+  let sin = Math.sin(frame),
+    x = Math.round(sin * 10 + 10),
+    char = Math.abs(sin) > 0.85 ? "*" : Math.cos(frame) > 0 ? "-" : "@"
   return line.slice(0, x) + char + line.slice(x + 1)
 }
 
 for (let frame = 0;;frame++) {
   await new Promise((resolve) => setTimeout(resolve, 60))
-  let line = addSinToLine(" ".repeat(25), frame + 15, 0.2, 10)
-  console.log(addSinToLine(line, frame, 0.21, 10))
+  let line = addSinToLine(" ".repeat(25), (frame + 15) * 0.2)
+  console.log(addSinToLine(line, frame * 0.21))
 }
